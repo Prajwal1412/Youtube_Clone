2dd72ca85927fc95d24ab1e6298f65640ed1ef33
prajwal1412 <prajwalwagh14@gmail.com>
2025-09-17 13:17:04 +0530

    Resolved TypeError: Cannot read properties of undefined (reading 'map') at Feed (Feed.jsx:35:16) by fixing feed.jsx file

--- Frontend/components/Sections/Feed.jsx
+++ Frontend/components/Sections/Feed.jsx
@@ -1,42 +1,42 @@
-import { useContext, useState, useEffect } from "react"
-import Videolayout from "./Videolayout"
+import { useContext, useState, useEffect } from "react";
+import Videolayout from "./Videolayout";
 import CategoryContext from "../../src/Context/CategoryContext";
 /*This function component loops through the list of objects which we get from the api 
  and create the videolayout based on the data received to show on the homepage */
-export default function Feed(){
-    // creating a useState for setting the videos
-    const [videos, setVideos] = useState([]);
-    // using useContext to get the category and displaying the video based on category
-   const { category, setCategory } = useContext(CategoryContext);
+export default function Feed() {
+  // creating a useState for setting the videos
+  const [videos, setVideos] = useState([]);
+  // using useContext to get the category and displaying the video based on category
+  const { category, setCategory } = useContext(CategoryContext);
 
-// using useEffect to fetch the videos
-    useEffect(()=>{
-        const fetchVideos = async() =>{
-            try{
-                const data = import.meta.env.VITE_Youtube_Api_key;             
-                // fetching the videos
-  const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=30&regionCode=US&videoCategoryId=${category}&key=${data} `)
-  const result = await response.json();
+  // using useEffect to fetch the videos
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const data = import.meta.env.VITE_Youtube_Api_key;
+        // fetching the videos
+        const response = await fetch(
+          `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=30&regionCode=US&videoCategoryId=${category}&key=${data} `
+        );
+        const result = await response.json();
 
-  // storing the videos
-    const items = result.items;
-  
- 
-setVideos(items);
+        // storing the videos
+        const items = result.items;
 
+        setVideos(items);
       } catch (error) {
         console.error("Failed to fetch YouTube videos", error);
       }
     };
-    fetchVideos()
-    },[category])
-    return(
-        <div className="Thumbnail-container">
-       {videos.map((video, index) => (
-        <Videolayout key={index} video={video} />
-      ))}
-
-        </div>
-      
-    )
+    fetchVideos();
+  }, [category]);
+  return (
+    <div className="Thumbnail-container">
+      {videos && videos.length > 0 ? (
+        videos.map((video, index) => <Videolayout key={index} video={video} />)
+      ) : (
+        <p>No videos available</p>
+      )}
+    </div>
+  );
 }
